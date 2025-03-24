2392a53e7ed16666ab8e6049692334366cd04a89
Diogo Demetrius <diogod.mmoreira@gmail.com>
2025-03-24 20:37:28 -0300

    dominio cors vercel

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://b2agroforms.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
